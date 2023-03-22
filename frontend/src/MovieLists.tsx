22f6108c3c21698fd8f7bc0aeaf26b5641c7d40b
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2023-03-22 18:08:40 +0200

    frontend<> adding links to each movie

--- frontend/src/MovieLists.tsx
+++ frontend/src/MovieLists.tsx
@@ -51,20 +51,21 @@ const MovieList = () => {
         // styling the cards that include the poster, the title, and the review of the movie 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8"> 
           {filteredMovies.map((movie: Movie) => (
-            // displaying the image, review, and the title whose background is black
-            <div key={movie.id} className=" card bg-black rounded-lg overflow-hidden hover:opacity-60 relative">
-              <div className='front'>
-                <img
-                  className="w-full h-65 object-cover"
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt={movie.title}
-                />
-              </div>
-              <div className=" p-4  top-0 left-0 w-full h-full opacity-0 hover:opacity-70 transition-opacity duration-300">
-                <h2 className="text-lg font-bold mb-2">{movie.title}</h2>
-                <p className="text-black-700">{movie.overview}</p>
-              </div>
-            </div>
+             <a key={movie.id} href={`https://www.themoviedb.org/movie/${movie.id}`} target="_blank">
+               <div key={movie.id} className=" card bg-black rounded-lg overflow-hidden hover:opacity-60 relative">
+                  <div className='front'>
+                    <img
+                      className="w-full h-65 object-cover"
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                  </div>
+                  <div className=" p-4  top-0 left-0 w-full h-full opacity-0 hover:opacity-70 transition-opacity duration-300">
+                    <h2 className="text-lg font-bold mb-2">{movie.title}</h2>
+                    <p className="text-black-700">{movie.overview}</p>
+                  </div>
+                </div>
+            </a>
           ))}
         </div>
          ) : (
