29395555dee14ffd1463e403be9594a624bf430f
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2023-03-22 13:46:27 +0200

    frontend<>defining the api by fetching the data through react query and axios

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -8,3 +8,10 @@ export const getMovies = async () => {
   );
   return response.data.results;
 };
+
+export const searchMovies = async (query: string) => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1`
+  );
+  return response.data.results;
+};
