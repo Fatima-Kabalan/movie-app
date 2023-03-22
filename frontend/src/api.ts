26c003523c34182df3bbab25ee898a35ed2ca7de
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2023-03-22 11:57:27 +0200

    frontend<>adding api file that gets the data from the database

---
+++ frontend/src/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_KEY = '7ddaebc6d7da9611149903cc8f5663c1';
+
+export const getMovies = async () => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return response.data.results;
+};
