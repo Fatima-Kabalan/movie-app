1f48164ba6cf0207b794473f5d2f4783ff936801
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2023-03-22 13:42:34 +0200

    frontend<>adding react query and search results using hooks in App file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -67,59 +67,79 @@
 
 
 // export default App;
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import MovieList from "./MovieLists";
+// import { useState } from "react";
+// import { useForm } from "react-hook-form";
+// import axios from "axios";
+// import MovieList from "./MovieLists";
 
-type Movie = {
-  id: number;
-  title: string;
-  overview: string;
-  poster_path: string;
-  release_date: string;
-};
+// type Movie = {
+//   id: number;
+//   title: string;
+//   overview: string;
+//   poster_path: string;
+//   release_date: string;
+// };
 
-type FormData = {
-  search: string;
-};
+// type FormData = {
+//   search: string;
+// };
+
+// function App() {
+//   const [searchResults, setSearchResults] = useState<Movie[]>([]);
+//   const { register, handleSubmit, reset } = useForm<FormData>();
+
+//   const handleSearch = async ({ search }: FormData) => {
+//     try {
+//       const response = await axios.get(
+//         `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=7ddaebc6d7da9611149903cc8f5663c1`
+//       );
+//       setSearchResults(response.data.results);
+//       reset();
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <div className="p-4">
+//       <form onSubmit={handleSubmit(handleSearch)} className="flex gap-2">
+//         <input
+//           type="text"
+//           name="search"
+//           placeholder="Search movies..."
+//           className="border border-gray-300 p-2 rounded"
+//           // ref={register({ required: true })}
+//         />
+//         <button
+//           type="submit"
+//           className="bg-blue-500 text-white px-4 py-2 rounded"
+//         >
+//           Search
+//         </button>
+//       </form>
+//       <MovieList movies={searchResults} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import MovieList from './MovieLists';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [searchResults, setSearchResults] = useState<Movie[]>([]);
-  const { register, handleSubmit, reset } = useForm<FormData>();
-
-  const handleSearch = async ({ search }: FormData) => {
-    try {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=7ddaebc6d7da9611149903cc8f5663c1`
-      );
-      setSearchResults(response.data.results);
-      reset();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <div className="p-4">
-      <form onSubmit={handleSubmit(handleSearch)} className="flex gap-2">
-        <input
-          type="text"
-          name="search"
-          placeholder="Search movies..."
-          className="border border-gray-300 p-2 rounded"
-          // ref={register({ required: true })}
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Search
-        </button>
-      </form>
-      <MovieList movies={searchResults} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="bg-gray-900 text-gray-100 min-h-screen">
+        <MovieList />
+      </div>
+    </QueryClientProvider>
   );
 }
 
 export default App;
+
