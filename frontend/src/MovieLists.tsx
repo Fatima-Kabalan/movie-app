fdf9c80474946ed09857a2010114ae0a55755acd
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2023-03-22 14:10:50 +0200

    frontend<>changing the button's style

--- frontend/src/MovieLists.tsx
+++ frontend/src/MovieLists.tsx
@@ -32,7 +32,7 @@ const MovieList = () => {
             placeholder="Search movies..."
           />
           <button
-            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            className="flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-sm border-4 text-white py-1 px-2 rounded"
             type="submit"
           >
             Search
